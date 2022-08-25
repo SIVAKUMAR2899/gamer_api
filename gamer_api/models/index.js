eb5bab4b66710a9245166f20ae924772b40ef11d
sivakumar <nsivakumar9982@gmail.com>
2022-08-25 19:27:00 +0530

    command sequelize

--- gamer_api/models/index.js
+++ gamer_api/models/index.js
@@ -1,24 +1,24 @@
 // const sequelize = require('sequelize');
 const dbconfig = require('../config/dbconfig');
 
-const {Sequelize,DataTypes} = require('sequelize');
+// const {Sequelize,DataTypes} = require('sequelize');
 
-const sequelize = new Sequelize(
-    dbconfig.DB,
-    dbconfig.USER,
-    dbconfig.PASSWORD,{
-        host: dbconfig.HOST,
-        dialect: dbconfig.dialect,
-        // operatorsAliases: false,
+// const sequelize = new Sequelize(
+//     dbconfig.DB,
+//     dbconfig.USER,
+//     dbconfig.PASSWORD,{
+//         host: dbconfig.HOST,
+//         dialect: dbconfig.dialect,
+//         // operatorsAliases: false,
 
-        pool:{
-            max: dbconfig.pool.max,
-            min: dbconfig.pool.min,
-            acquire: dbconfig.pool.acquire,
-            idle: dbconfig.pool.idle
-        }
-    }
-)
+//         pool:{
+//             max: dbconfig.pool.max,
+//             min: dbconfig.pool.min,
+//             acquire: dbconfig.pool.acquire,
+//             idle: dbconfig.pool.idle
+//         }
+//     }
+// )
 
 // sequelize.authenticate()
 // .then(()=>{
@@ -34,8 +34,8 @@ const db = {}
 // db.sequelize = sequelize
 
 
-db.users = require('./usermodel')(sequelize, DataTypes)
-db.skills = require('./skillmodel')(sequelize, DataTypes)
+// db.users = require('./usermodel')(sequelize, DataTypes)
+// db.skills = require('./skillmodel')(sequelize, DataTypes)
 
 // db.sequelize.sync({ force: false })
 // .then(() => {
