a131cfae4c8d6569aceebad4223b27ad383a1f3e
sivakumar <nsivakumar9982@gmail.com>
2022-08-30 12:42:00 +0530

    command sequelize

--- gamer_api/server.js
+++ gamer_api/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-// const lowdb = require('lowdb')
 const morgan = require("morgan");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
+const router = require('./routes/playersRouter')
 // const db = require("./config/dbconfig")
 // var corOptions = {
 //     origin: "http://localhost:5000"
 // }
 
-const router = require('./routes/playersRouter')
+const PORT = process.env.PORT || 5000
 
 const options ={
   definition:{
@@ -34,22 +34,24 @@ const app = express();
 
 // app.use("/", router);
 
-// app.use('/api-docs/api/gamer/users', router)
-// app.use('/api-docs/api/gamer/skills', router)
+// app.use('/api/gamer/users', router)
+// app.use('/api/gamer/skills', router)
 
 
-app.get('/',(req,res) => {
-  res.send("GAMER");
-}); 
+// app.get('/',(req,res) => {
+//   res.send("GAMER");
+// }); 
+
 
 app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(specs));
 
 // app.use(cors(corOptions)); 
+app.use(cors()); 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: true}))
 
-const PORT = process.env.PORT || 5000
+
 
 app.use('/playersRouter',router)
 
